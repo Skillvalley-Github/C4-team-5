067e19588ca73ad373b175d1326e70626fc86587
1-Shreyash <108738197+1-Shreyash@users.noreply.github.com>
2023-09-11 00:42:57 +0530

    Done

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import { Sidebar } from './Components/Sidebar';
+import { Rbody } from './Components/Rbody';
+
+function App() {
+  return (
+    <>
+    <div className="main">
+      <Sidebar/>
+      <Rbody/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Rbody.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from './Header';
+
+export const Rbody = () => {
+  return (
+    <>
+        <div className="Rbody">
+            <Header name = "Sohidur Rahman" overview="Dashboard"/>
+        </div>
+        
+    </>
+  )
+}
